fa4b2b0e67df1831dc4339bcfa5ecbd479072077
Juan Pablo Garcia Plaza Perez <jnpblgpz@gmail.com>
2024-11-13 01:39:51 +0100

    Fix Stage 3 DB queries parsing

--- Musicosa Runner/stage_3_pre_templates_gen/stage_input.py
+++ Musicosa Runner/stage_3_pre_templates_gen/stage_input.py
@@ -14,7 +14,7 @@ def load_unfulfilled_contestants_from_db() -> list[Contestant]:
 
 
 def load_entries_index_of_unfulfilled_templates_from_db() -> dict[int, Entry]:
-    return dict([(result.ranking_sequence, result.entry.to_domain())
+    return dict([(result.orm.ranking_sequence, result.orm.entry.to_domain())
                  for result in
                  Entry.ORM
                 .select(EntryStats.ORM.ranking_sequence, EntryStats.ORM.entry)
@@ -22,12 +22,11 @@ def load_entries_index_of_unfulfilled_templates_from_db() -> dict[int, Entry]:
                       on=(Entry.ORM.id == EntryStats.ORM.entry))
                 .join(Template.ORM, join_type=JOIN.LEFT_OUTER,
                       on=(Entry.ORM.id == Template.ORM.entry))
-                .where(Template.ORM.entry.is_null())
-                .objects()])
+                .where(Template.ORM.entry.is_null())])
 
 
 def load_entries_index_of_unfulfilled_video_options_from_db() -> dict[int, Entry]:
-    return dict([(result.ranking_sequence, result.entry.to_domain())
+    return dict([(result.orm.ranking_sequence, result.orm.entry.to_domain())
                  for result in
                  Entry.ORM
                 .select(EntryStats.ORM.ranking_sequence, EntryStats.ORM.entry)
@@ -35,8 +34,7 @@ def load_entries_index_of_unfulfilled_video_options_from_db() -> dict[int, Entry
                       on=(Entry.ORM.id == EntryStats.ORM.entry))
                 .join(VideoOptions.ORM, join_type=JOIN.LEFT_OUTER,
                       on=(Entry.ORM.id == VideoOptions.ORM.entry))
-                .where(VideoOptions.ORM.entry.is_null())
-                .objects()])
+                .where(VideoOptions.ORM.entry.is_null())])
 
 
 def load_musicosa_from_db() -> Musicosa:
