28a95a1933747c616a3f46ea4bc835964e290d91
Juan Pablo Garcia Plaza Perez <jnpblgpz@gmail.com>
2025-09-06 19:39:17 +0200

    Add imports sort via Prettier plugin

--- Musicosa Template Server/prettier.config.mjs
+++ Musicosa Template Server/prettier.config.mjs
@@ -1,6 +1,5 @@
 /**
- * @see https://prettier.io/docs/configuration
- * @type {import("prettier").Config}
+ * @type {import("prettier").Config & import("@trivago/prettier-plugin-sort-imports").PluginConfig}
  */
 const config = {
     printWidth: 120,
@@ -20,6 +19,20 @@ const config = {
     endOfLine: 'auto',
     embeddedLanguageFormatting: 'auto',
     singleAttributePerLine: true,
+    plugins: ['@trivago/prettier-plugin-sort-imports'],
+    importOrder: [
+        // 1) third-party packages (scoped and unscoped)
+        '^@?\\w',
+        // 2) path-alias imports
+        '^@/',
+        // 3) same-directory and index
+        '^\\./(?=.*/)(?!/?$)',
+        '^\\.(?!/?$)',
+        '^\\./?$',
+    ],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    importOrderSideEffects: true,
 }
 
 export default config
